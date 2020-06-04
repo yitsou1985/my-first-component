de7e1b2e20b8d78f257f00f2b5d6e378237ece08
yitsou1985 <57088413+yitsou1985@users.noreply.github.com>
2020-06-04 14:55:33 +0300

    Updates

---
+++ precache-manifest.e6c0e39be49e5fa264c3559f50f22f8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d7a4241cdecb950abd65ba8a002a6d",
+    "url": "/my-first-component/index.html"
+  },
+  {
+    "revision": "0080927aef7279366444",
+    "url": "/my-first-component/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e7fe33e907cb65943a81",
+    "url": "/my-first-component/static/css/main.bf8830a4.chunk.css"
+  },
+  {
+    "revision": "0080927aef7279366444",
+    "url": "/my-first-component/static/js/2.22c80422.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-first-component/static/js/2.22c80422.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7fe33e907cb65943a81",
+    "url": "/my-first-component/static/js/main.9ca927b1.chunk.js"
+  },
+  {
+    "revision": "74d9f8084cafb35e67a5",
+    "url": "/my-first-component/static/js/runtime-main.b940583e.js"
+  }
+]);
